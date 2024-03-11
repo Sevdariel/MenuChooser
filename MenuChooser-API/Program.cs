f8792b9d231e971a0e2ac0800dc9a96192a4deb5
Michał Zalewski <michal.zalewski1995@gmail.com>
2024-03-11 22:08:06 +0100

    Remove comments

--- MenuChooser-API/Program.cs
+++ MenuChooser-API/Program.cs
@@ -2,17 +2,14 @@ using MenuChooser.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 builder.Services.AddApplicationService(builder.Configuration);
 
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,7 +17,5 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-//app.UseAuthorization();
 
 app.Run();
