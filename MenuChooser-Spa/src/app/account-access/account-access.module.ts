b51f23e59b1ddf81d4ec859e7d9f2244f086ce5e
Michał Zalewski <michal.zalewski1995@gmail.com>
2024-05-11 13:58:50 +0200

    AccountAccessModules fixes
    Add AccountAccessModule to update index

--- MenuChooser-Spa/src/app/account-access/account-access.module.ts
+++ MenuChooser-Spa/src/app/account-access/account-access.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AccountAccessRoutingModule } from './account-access-routing.module';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ErrorDirective } from '../core/validation/error-directive/error.directive';
 
 @NgModule({
   declarations: [
     LoginComponent,
   ],
   imports: [
+    ErrorDirective,
     CommonModule,
     AccountAccessRoutingModule,
     ReactiveFormsModule,
