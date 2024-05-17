107d49b1718640ed24cd05b43ca1a9fa2dd3a763
Michał Zalewski <michal.zalewski1995@gmail.com>
2024-05-17 11:35:25 +0200

    Add user storage

--- MenuChooser-Spa/src/app/shared/account/account.service.ts
+++ MenuChooser-Spa/src/app/shared/account/account.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
-import { filter, map } from 'rxjs';
+import { filter, tap } from 'rxjs';
 import { IUser, IUserLoginDto } from './account-dto.model';
 
 @Injectable({
@@ -8,8 +8,8 @@ import { IUser, IUserLoginDto } from './account-dto.model';
 })
 export class AccountService {
 
-  private baseUrl = `api/account/`
-  private currentUser = signal({});
+  private baseUrl = `api/account`
+  private currentUser = signal<IUser | undefined>(undefined);
   public loggedUser = this.currentUser.asReadonly();
 
   constructor(private httpClient: HttpClient) { }
@@ -17,8 +17,18 @@ export class AccountService {
   public login(userDto: IUserLoginDto) {
     return this.httpClient.post<IUser>(`${this.baseUrl}/login`, userDto)
       .pipe(
-        filter(logedUser => !!logedUser),
-        map(logedUser => this.setCurrentUser(logedUser)));
+        filter(loggedUser => !!loggedUser),
+        tap(loggedUser => this.setCurrentUser(loggedUser)),
+        tap(loggedUser => this.setStorageUser(loggedUser, userDto.rememberMe)));
+
+  }
+
+  private setStorageUser(user: IUser, rememberMe: boolean) {
+    if (rememberMe) {
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      sessionStorage.setItem('user', JSON.stringify(user));
+    }
   }
 
   private setCurrentUser(user: IUser) {
