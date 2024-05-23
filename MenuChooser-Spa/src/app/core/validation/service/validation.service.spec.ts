43ee337efc3f69ee2d946b40ca4186bf7e586c9d
Michał Zalewski <michal.zalewski1995@gmail.com>
2024-05-23 13:59:35 +0200

    Add ValidationService tests

--- MenuChooser-Spa/src/app/core/validation/service/validation.service.spec.ts
+++ MenuChooser-Spa/src/app/core/validation/service/validation.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ValidationService } from './validation.service';
+import { FormControl, Validators } from '@angular/forms';
+import { errorMessages } from './error-messages';
 
 describe('ValidationService', () => {
-  let service: ValidationService;
+  let validationService: ValidationService;
+  let formControl: FormControl;
+  let parentName: 'formControlName';
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ValidationService);
+    validationService = TestBed.inject(ValidationService);
+
+    formControl = new FormControl();
+
+    formControl.setValidators(Validators.required);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(validationService).toBeTruthy();
   });
+
+  it('showError should return false when control has required validator and set value', () => {
+    formControl.setValue('kappa');
+    formControl.markAsTouched();
+
+    expect(validationService.showError(formControl)).toEqual(false);
+  })
+
+  it('showError should return false when control is invalid and is not touched', () => {
+    formControl.setValue('');
+
+    expect(validationService.showError(formControl)).toEqual(false);
+  })
+
+  it('showError should return true when control is invalid and touched', () => {
+    formControl.setValue('');
+    formControl.markAsTouched();
+
+    expect(validationService.showError(formControl)).toEqual(true);
+  })
+
+  it('getErrorMessage should return null when control is valid', () => {
+    formControl.setValue('kappa');
+
+    expect(validationService.getErrorMessage(formControl)).toBeNull();
+  })
+
+  it('getErrorMessage should return required error message when value is required', () => {
+    formControl.setValue('');
+
+    expect(validationService.getErrorMessage(formControl)).toEqual(errorMessages['required']);
+  })
+
+  it('getErrorMessage should replace placeholder when it exist and parentName is given', () => {
+    formControl.setErrors({ sameValues: true });
+
+    expect(validationService.getErrorMessage(formControl, parentName)).toEqual(errorMessages['sameValues']);
+  })
 });
