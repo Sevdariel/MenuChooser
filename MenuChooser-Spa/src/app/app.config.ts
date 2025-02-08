9ff3fd02238b63b02e3856b25a3c7ba803e18022
Michał Zalewski <michal.zalewski1995@gmail.com>
2025-02-08 16:37:15 +0100

    Little fixes

--- MenuChooser-Spa/src/app/app.config.ts
+++ MenuChooser-Spa/src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAngularSvgIcon } from 'angular-svg-icon';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { errorInterceptor } from './core/interceptors/error-interceptor';
-import { provideAngularSvgIcon } from 'angular-svg-icon';
 
 export const appConfig: ApplicationConfig = {
   providers: [
