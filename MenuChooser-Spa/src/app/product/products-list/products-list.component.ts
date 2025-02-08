86525a12b6f6ca91dc705d86736abce8ded78293
Michał Zalewski <michal.zalewski1995@gmail.com>
2025-02-08 16:20:06 +0100

    Add product table

--- MenuChooser-Spa/src/app/product/products-list/products-list.component.ts
+++ MenuChooser-Spa/src/app/product/products-list/products-list.component.ts
@@ -1,18 +1,17 @@
-import { AfterViewInit, Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { tap } from 'rxjs';
 import { IProduct } from '../models/product.model';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'mc-products-list',
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.scss'
 })
-export class ProductsListComponent implements OnInit, AfterViewInit {
-  ngAfterViewInit(): void {
-    console.log('this.products()', this.products());
-  }
+export class ProductsListComponent implements OnInit {
+
+  public readonly columns = ['Name', 'Producent', 'CreatedBy'];
 
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly destroyRef = inject(DestroyRef);
