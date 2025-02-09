e8229e342642883faf92708f032167f2607e89a3
Michał Zalewski <michal.zalewski1995@gmail.com>
2025-02-09 14:25:50 +0100

    Conception change
    View will be readonly and edit panel will show

--- MenuChooser-Spa/src/app/product/views/product/product.component.ts
+++ MenuChooser-Spa/src/app/product/views/product/product.component.ts
@@ -2,9 +2,8 @@ import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { tap } from 'rxjs';
-import { IProduct, ProductFormType } from '../../models/product.model';
-import { FormBuilder, FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { defaultProduct } from '../../models/default-product.model';
+import { IProduct } from '../../models/product.model';
 
 @Component({
   selector: 'mc-product',
@@ -15,25 +14,18 @@ export class ProductComponent implements OnInit {
 
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly destroyRef = inject(DestroyRef);
-  private readonly formBuilder = inject(FormBuilder);
 
   private productSignal = signal<IProduct>(defaultProduct);
   public product = this.productSignal.asReadonly();
 
-  public formGroup!: FormGroup<ProductFormType>;
-
   public ngOnInit(): void {
     this.activatedRoute.data.pipe(
       tap(data => this.productSignal.set(data['product'])),
-      tap(() => this.initFormGroup()),
       takeUntilDestroyed(this.destroyRef))
       .subscribe();
   }
 
-  private initFormGroup() {
-    this.formGroup = this.formBuilder.group<ProductFormType>({
-      name: new FormControl(this.product().name),
-      producent: new FormControl(this.product().producent),
-    })
+  public edit() {
+    
   }
 }
