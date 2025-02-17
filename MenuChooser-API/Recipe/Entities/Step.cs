c413cea60a0516537b5d96cb858047fcc11b4b22
Michał Zalewski <michal.zalewski1995@gmail.com>
2025-02-17 12:52:12 +0100

    Add recipe and step entity

---
+++ MenuChooser-API/Recipe/Entities/Step.cs
@@ -0,0 +1,17 @@
+﻿using Products.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Entities
+{
+    public class Step
+    {
+        public string Content { get; set; } = null!;
+        public List<Product> Products { get; set; } = null!;
+        public int Duration { get; set; }
+
+    }
+}
