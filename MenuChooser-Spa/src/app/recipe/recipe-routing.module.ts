6ae66adc5acd6b93fa870c2940b5071c91167462
Michał Zalewski <michal.zalewski1995@gmail.com>
2025-02-18 15:25:34 +0100

    Add recipes component

--- MenuChooser-Spa/src/app/recipe/recipe-routing.module.ts
+++ MenuChooser-Spa/src/app/recipe/recipe-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesListComponent } from './views/recipes-list/recipes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RecipesListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ MenuChooser-Spa/src/app/recipe/views/recipes-list/recipes-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-recipes-list',
+  imports: [],
+  templateUrl: './recipes-list.component.html',
+  styleUrl: './recipes-list.component.scss'
+})
+export class RecipesListComponent {
+
+}
