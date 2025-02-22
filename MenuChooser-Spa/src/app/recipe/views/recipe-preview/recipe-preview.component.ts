66834c347c829cdea2b1db5768c06dc17179b644
Michał Zalewski <michal.zalewski1995@gmail.com>
2025-02-22 17:16:12 +0100

    Get data via resolver

--- MenuChooser-Spa/src/app/recipe/views/recipe-preview/recipe-preview.component.ts
+++ MenuChooser-Spa/src/app/recipe/views/recipe-preview/recipe-preview.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, inject, OnInit, signal, DestroyRef } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { defaultRecipe } from '../../models/default-recipe.model';
+import { IRecipe } from '../../models/recipe.model';
+import { tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'mc-recipe-preview',
@@ -9,6 +14,17 @@ import { CardModule } from 'primeng/card';
   templateUrl: './recipe-preview.component.html',
   styleUrl: './recipe-preview.component.scss'
 })
-export class RecipePreviewComponent {
+export class RecipePreviewComponent implements OnInit {
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly destroyRef = inject(DestroyRef);
 
+  private recipeSignal = signal<IRecipe>(defaultRecipe);
+  public recipe = this.recipeSignal.asReadonly();
+
+  public ngOnInit(): void {
+    this.activatedRoute.data.pipe(
+      tap(data => this.recipeSignal.set(data['recipe'])),
+      takeUntilDestroyed(this.destroyRef))
+      .subscribe();
+  }
 }
