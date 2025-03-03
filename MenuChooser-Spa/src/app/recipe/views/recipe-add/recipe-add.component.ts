cc4ba204b2fa6b3a5203382d96f091fa985f2358
Michał Zalewski <michal.zalewski1995@gmail.com>
2025-03-03 20:13:02 +0100

    Create component

---
+++ MenuChooser-Spa/src/app/recipe/views/recipe-add/recipe-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-recipe-add',
+  imports: [],
+  templateUrl: './recipe-add.component.html',
+  styleUrl: './recipe-add.component.scss'
+})
+export class RecipeAddComponent {
+
+}
