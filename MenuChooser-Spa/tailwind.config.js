4fa99c6ed255090cd2bf126d4a83d5c2c32feb77
Michał Zalewski <michal.zalewski1995@gmail.com>
2025-10-20 19:18:41 +0200

    Tailwind with primeng fixes

--- MenuChooser-Spa/tailwind.config.js
+++ MenuChooser-Spa/tailwind.config.js
@@ -5,25 +5,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        'light-orange': {
-          0: '#FFF6DE',
-          100: '#FFF3D2',
-          200: '#FFEABB',
-          300: '#FFE1A4',
-          400: '#FFD88D',
-          500: '#FFCF76',
-          600: '#FFC65F',
-          700: '#FFBD48',
-          800: '#FFB431',
-          900: '#FFAB19',
-        }
-      }
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('tailwindcss-primeui'),
   ],
 }
 
