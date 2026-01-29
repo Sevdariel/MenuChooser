31c066795c419182ad734a80b83540a9fa4bc037
Sev <michal.zalewski1995@gmail.com>
2026-01-29 23:07:17 +0100

    Format loginComponent

--- MenuChooser-Spa/src/app/account/login/login.component.ts
+++ MenuChooser-Spa/src/app/account/login/login.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { ErrorDirective } from '../../core/validation/error-directive/error.directive';
 import { ValidationService } from '../../core/validation/service/validation.service';
@@ -9,32 +15,26 @@ import { IUserLoginDto } from '../../shared/account/account-dto.model';
 import { AccountService } from '../../shared/account/account.service';
 
 @Component({
-    selector: 'mc-login',
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.scss',
-    standalone: true,
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        RouterLink,
-        ErrorDirective
-    ]
+  selector: 'mc-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, ErrorDirective],
 })
 export class LoginComponent implements OnInit {
-
-  public formGroup!: FormGroup;
+  public validationService = inject(ValidationService);
+  private formBuilder = inject(FormBuilder);
+  private accountService = inject(AccountService);
+  private router = inject(Router);
   private destroyRef = inject(DestroyRef);
 
-  constructor(
-    public validationService: ValidationService,
-    private formBuilder: FormBuilder,
-    private accountService: AccountService,
-    private router: Router,
-  ) { }
+  public formGroup!: FormGroup;
 
   public ngOnInit(): void {
     this.formGroup = this.formBuilder.nonNullable.group({
-      email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+      }),
       password: new FormControl('', { validators: Validators.required }),
       rememberMe: new FormControl(false),
     });
@@ -44,10 +44,10 @@ export class LoginComponent implements OnInit {
     this.formGroup.markAllAsTouched();
 
     if (this.formGroup.valid) {
-      this.accountService.login(this.createUserLoginDto())
-        .pipe(
-          takeUntilDestroyed(this.destroyRef),
-        ).subscribe(() => this.router.navigateByUrl(''));
+      this.accountService
+        .login(this.createUserLoginDto())
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe(() => this.router.navigateByUrl(''));
     }
   }
 
@@ -55,7 +55,7 @@ export class LoginComponent implements OnInit {
     return {
       email: this.formGroup.controls['email'].value,
       password: this.formGroup.controls['password'].value,
-      rememberMe: this.formGroup.controls['rememberMe'].value
+      rememberMe: this.formGroup.controls['rememberMe'].value,
     };
   }
 }
