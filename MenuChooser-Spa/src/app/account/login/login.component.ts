d105eefcd1dacf2efd9664c5fce0f2bcd661ef9d
Sev <michal.zalewski1995@gmail.com>
2026-02-01 00:42:49 +0100

    Work with signal forms

--- MenuChooser-Spa/src/app/account/login/login.component.ts
+++ MenuChooser-Spa/src/app/account/login/login.component.ts
@@ -1,24 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import {
-  email,
-  FieldTree,
-  form,
-  FormField,
-  required,
-} from '@angular/forms/signals';
+import { ReactiveFormsModule } from '@angular/forms';
+import { email, form, FormField, required } from '@angular/forms/signals';
 import { Router, RouterLink } from '@angular/router';
 import { ErrorDirective } from '../../core/validation/error-directive/error.directive';
 import { ValidationService } from '../../core/validation/service/validation.service';
-import { IUserLoginDto } from '../../shared/account/account-dto.model';
 import { AccountService } from '../../shared/account/account.service';
 import { ILogin } from './models/login.model';
 
@@ -34,14 +21,11 @@ import { ILogin } from './models/login.model';
     FormField,
   ],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   public validationService = inject(ValidationService);
-  private formBuilder = inject(FormBuilder);
   private accountService = inject(AccountService);
   private router = inject(Router);
   private destroyRef = inject(DestroyRef);
-
-  public formGroup!: FormGroup;
 
   private loginModel = signal<ILogin>({
     email: '',
@@ -57,52 +41,13 @@ export class LoginComponent implements OnInit {
       required(schemaPath.password, { message: 'Password is required' }));
   });
 
-  public ngOnInit(): void {
-    this.formGroup = this.formBuilder.nonNullable.group({
-      email: new FormControl('', {
-        validators: [Validators.required, Validators.email],
-      }),
-      password: new FormControl('', { validators: Validators.required }),
-      rememberMe: new FormControl(false),
-    });
-  }
-
   public login() {
-    console.log('this.signalForm', this.signalForm);
-    console.log('this.signalForm.email()', this.signalForm.email());
-    console.log('this.signalForm.password()', this.signalForm.password());
-    console.log('this.signalForm.rememberMe()', this.signalForm.rememberMe());
-    console.log('this.signalForm.email().valu', this.signalForm.email().value);
-    console.log(
-      'this.signalForm.password().value',
-      this.signalForm.password().value,
-    );
-    console.log(
-      'this.signalForm.rememberMe().value',
-      this.signalForm.rememberMe().value,
-    );
-
-    console.log('this.signalForm.email().errors()', this.signalForm.email().errors());
-    if (this.signalForm().invalid()) {
-      console.log('this.loginModel()', this.loginModel());
-    } else {
-      console.log('Walidacja przeszłą zajebiście')
-    }
-    this.formGroup.markAllAsTouched();
-
-    if (this.formGroup.valid) {
+    if (this.signalForm().valid()) {
+      const loginDto = this.loginModel();
       this.accountService
-        .login(this.createUserLoginDto())
+        .login(loginDto)
         .pipe(takeUntilDestroyed(this.destroyRef))
         .subscribe(() => this.router.navigateByUrl(''));
     }
-  }
-
-  private createUserLoginDto(): IUserLoginDto {
-    return {
-      email: this.formGroup.controls['email'].value,
-      password: this.formGroup.controls['password'].value,
-      rememberMe: this.formGroup.controls['rememberMe'].value,
-    };
   }
 }
