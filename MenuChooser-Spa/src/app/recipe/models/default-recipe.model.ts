3b144c4a021b114325cda155dbf3be19ae05fd19
Sev <michal.zalewski1995@gmail.com>
2026-02-15 23:31:50 +0100

    Implement state to recipes
    GetRecipes method

--- MenuChooser-Spa/src/app/recipe/models/default-recipe.model.ts
+++ MenuChooser-Spa/src/app/recipe/models/default-recipe.model.ts
@@ -1,11 +1,19 @@
-import { IRecipe } from "./recipe.model";
+import { IRecipeListItem } from './recipe-dto.model';
+import { IRecipe, MealType } from './recipe.model';
 
 export const defaultRecipe: IRecipe = {
-    createdBy: '',
-    duration: 0,
-    id: '',
-    name: '',
-    products: [],
-    steps: [],
-    updatedBy: '',
-}
+  createdBy: '',
+  duration: 0,
+  id: '',
+  name: '',
+  products: [],
+  steps: [],
+  updatedBy: '',
+};
+
+export const defaultRecipeListItem: IRecipeListItem = {
+  id: '',
+  name: '',
+  duration: 0,
+  mealType: MealType.Appetizer,
+};
---
+++ MenuChooser-Spa/src/app/recipe/store/recipe.store.ts
@@ -0,0 +1,32 @@
+import { inject, Injectable } from '@angular/core';
+import { Action, State, StateContext } from '@ngxs/store';
+import { IRecipeListItem } from '../models/recipe-dto.model';
+import { RecipeService } from '../services/recipe.service';
+import { GetRecipes } from './recipe.actions';
+import { exhaustMap, tap } from 'rxjs';
+
+interface RecipeStateModel {
+  recipes: IRecipeListItem[];
+}
+
+@State<RecipeStateModel>({
+  name: 'recipe',
+  defaults: { recipes: [] },
+})
+@Injectable()
+export class RecipeState {
+  private readonly recipeService = inject(RecipeService);
+
+  @Action(GetRecipes)
+  public getRecipes(ctx: StateContext<RecipeStateModel>, action: GetRecipes) {
+    return this.recipeService.getRecipes().pipe(
+      tap((recipesResult) => {
+        const state = ctx.getState();
+        ctx.setState({
+          ...state,
+          recipes: recipesResult,
+        });
+      }),
+    );
+  }
+}
